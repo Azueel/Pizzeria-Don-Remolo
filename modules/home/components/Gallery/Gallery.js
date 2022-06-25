8d2b0173a0e189c541921bcac57c75dc4900efce
margaZM <aurazambrano227@gmail.com>
2022-06-25 17:42:46 -0500

    finish gallery section

--- modules/home/components/Gallery/Gallery.js
+++ modules/home/components/Gallery/Gallery.js
@@ -4,14 +4,14 @@ import React from 'react';
 export const Gallery = () => {
 	return (
 		<section className="bg-gallery w-full bg-center">
-			<section className="mx-auto leading-10 py-12 pb-20 sm:w-8/12">
+			<section className="mx-auto leading-10 py-12 pb-20 md:w-3/4">
 				<section className="flex justify-center">
 					<h2 className="text-center uppercase font-bold bg-secondary rounded-lg px-4 inline-block">
 						Conoce más de nosotros @Pizzeriadonremolo
 					</h2>
 				</section>
 
-				<section className="flex flex-wrap justify-center sm:justify-between items-center gap-5  mt-4">
+				<section className="flex flex-wrap justify-center md:justify-between items-center gap-2  mt-4">
 					<Image
 						src={require('/public/assets/gallery/picture1.png')}
 						alt="picture-don-remolo"
